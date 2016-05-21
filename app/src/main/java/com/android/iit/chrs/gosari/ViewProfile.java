befc6e7578277f7ecef6c43f9af5b18ebe94ec51
John Gregory D. Funtera <jgfuntera.chrs@gmail.com>
2016-05-21 17:30:14 +0800

    --Finished Changed Password

--- app/src/main/java/com/android/iit/chrs/gosari/ViewProfile.java
+++ app/src/main/java/com/android/iit/chrs/gosari/ViewProfile.java
@@ -7,8 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,7 +41,7 @@ public class ViewProfile extends Activity {
 
     JSONArray jsonArray;
 
-    Button btn_logout;
+    Button btn_logout,btn_changePass;
 
     DbHelper db;
 
@@ -47,6 +52,12 @@ public class ViewProfile extends Activity {
     String urlConnection;
 
     String acc_name,acc_email,acc_num;
+
+    EditText in_oldPass,in_newPass,in_confirmNewPass;
+
+    String oldpass,newpass,confirmpass;
+
+    //www.gosari.ph/api/users/change_password.php?mobile_number=09056075270&password=a
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +80,10 @@ public class ViewProfile extends Activity {
         tv_mobile=(TextView)findViewById(R.id.Profile_number);
         tv_emailAd=(TextView)findViewById(R.id.Profile_emailAd);
         btn_logout=(Button)findViewById(R.id.btn_logout);
+        btn_changePass=(Button)findViewById(R.id.btn_ChagePass);
+        in_oldPass=(EditText)findViewById(R.id.in_oldpassword);
+        in_newPass=(EditText)findViewById(R.id.in_newPassWord);
+        in_confirmNewPass=(EditText)findViewById(R.id.in_confirmnewPassWord);
 
         tv_name.setText(acc_name);
         tv_mobile.setText(acc_num);
@@ -82,12 +97,42 @@ public class ViewProfile extends Activity {
                 db.removeAllItem();
                 Log.e("MESSAGE: ","ACCOUNT DELETED");
                 Intent viewLogin = new Intent(getApplicationContext(),ViewLogIn.class);
+                finish();
                 startActivity(viewLogin);
+
+
+            }
+        });
+
+        btn_changePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  oldpass =in_oldPass.getText().toString();
+                  newpass=in_newPass.getText().toString();
+                  confirmpass=in_confirmNewPass.getText().toString();
+                if(ConfirmPassword()){
+                    Log.e("MESSAGE:","ACCOUNT CHANGE NEW PASS");
+                    HttpNewPassword(mobile,newpass);
+                    ClearFields();
+                    db.LogOut();
+                    db.clearchkouthistory();
+                    db.removeAllItem();
+                    Intent viewLogin = new Intent(getApplicationContext(),ViewLogIn.class);
+                    startActivity(viewLogin);
+                    finish();
+
+                }
+                else{
+                    Log.e("MESSAGE:","INVALID DETAILS");
+                    ClearFields();
+                }
+
 
             }
         });
 
     }
+
 
     public void HttpGetProfile(String mobile,String pass){
 
@@ -142,6 +187,72 @@ public class ViewProfile extends Activity {
             e.printStackTrace();
         }
     }
+
+
+    public boolean ConfirmPassword(){
+
+        boolean result=false;
+
+        if(oldpass!=null){
+
+            if(oldpass.equals(pass) && confirmpass.equals(newpass) && confirmpass != null){
+               result=true;
+           }
+        }
+
+        return result;
+
+
+    }
+
+
+    public void HttpNewPassword(String mobile,String newpass){
+        try {
+
+            HttpClient httpclient = new DefaultHttpClient();
+
+            HttpGet httpGet = new HttpGet("http://www.gosari.ph/api/users/change_password.php?mobile_number=" + mobile + "&password=" + newpass + "");
+
+            Log.e("MESSAGE", "http://www.gosari.ph/api/users/change_password.php?mobile_number=" + mobile + "&password=" + newpass + "");
+
+            HttpResponse response = httpclient.execute(httpGet);
+
+           if (response != null) {
+                InputStream is = response.getEntity().getContent();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                String message=sb.toString();
+                JSONObject jsonobj=new JSONObject(message);
+                String status=jsonobj.getString("status");
+                Log.e("STATUS: ",status);
+
+
+            }
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+      catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public void ClearFields(){
+        in_oldPass.setText("");
+        in_newPass.setText("");
+        in_confirmNewPass.setText("");
+    }
+
 
 
 
