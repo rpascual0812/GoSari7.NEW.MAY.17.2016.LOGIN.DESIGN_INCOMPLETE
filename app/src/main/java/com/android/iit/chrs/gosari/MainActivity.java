d7bd08edef920f66ef5347e62c7866ce55880b7a
John Gregory D. Funtera <jgfuntera.chrs@gmail.com>
2016-05-23 13:07:44 +0800

    --ADDED ANIMATION IN MAINACTIVITY.JAVA

--- app/src/main/java/com/android/iit/chrs/gosari/MainActivity.java
+++ app/src/main/java/com/android/iit/chrs/gosari/MainActivity.java
@@ -8,14 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
+
+    Animation slide_in,pop_in,pop_in2;
+
+    ImageView iv_buildings,iv_gosariIcons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv_buildings=(ImageView)findViewById(R.id.iv_buildings);
+        iv_gosariIcons=(ImageView)findViewById(R.id.iv_gosariIcon);
         btnStart=(Button)findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
                 ViewCategory();
             }
         });
+        Slide_in();
+       /* Pop_in2();
+        Pop_in();*/
+
 
 
     }
@@ -33,8 +46,48 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void Slide_in(){
+        slide_in=AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in);
+        iv_buildings.startAnimation(slide_in);
+
+        slide_in.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Pop_in2();
+                btnStart.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                    Pop_in();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    public void Pop_in(){
+        btnStart.setVisibility(View.VISIBLE);
+        pop_in=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.pop);
+        btnStart.startAnimation(pop_in);
+
+    }
+    public void Pop_in2(){
+        pop_in2=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.pop);
+        iv_gosariIcons.startAnimation(pop_in2);
+
+    }
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
     }
+
+
+
+
+
 }
